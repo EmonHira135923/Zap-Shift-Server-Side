6fdf5e8708fc98b5820ce3aa4985639a6a9e363d
EmonHira135923 <emonhossainhira231@gmail.com>
2026-01-16 01:34:21 +0600

    Users api get post update and delete api complete

--- src/controllers/usersCollection.js
+++ src/controllers/usersCollection.js
@@ -1,6 +1,8 @@
+import { ObjectId } from "mongodb";
 import { getUsers } from "../config/db.js";
 
 // Users Controller
+// post controller
 export const usersController = async (req, res) => {
   try {
     const usersCollection = getUsers();
@@ -20,6 +22,72 @@ export const usersController = async (req, res) => {
       success: false,
       message: "Could not create user",
       err: err.message,
+    });
+  }
+};
+
+// get Controller
+export const getUsersController = async (req, res) => {
+  try {
+    const usersCollection = getUsers();
+    const users = usersCollection.find().sort({ createdAt: -1 });
+    const result = await users.toArray();
+    return res.status(200).send({
+      success: true,
+      message: "Users fetched successfully",
+      result,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      err: err.message,
+      message: "Could not fetch users",
+    });
+  }
+};
+
+// update controller
+export const updateUserController = async (req, res) => {
+  try {
+    const usersCollection = getUsers();
+    const id = req.params.id;
+    const updateData = req.body;
+    const query = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: updateData.role,
+    };
+    const result = await usersCollection.updateOne(query, updateDoc);
+    return res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      result,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      err: err.message,
+      message: "Could not update user",
+    });
+  }
+};
+
+// Delete Controller
+export const deleteUserController = async (req, res) => {
+  try {
+    const usersCollection = getUsers();
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await usersCollection.deleteOne(query);
+    return res.status(200).send({
+      success: true,
+      message: "User deleted successfully",
+      result,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      err: err.message,
+      message: "Could not delete user",
     });
   }
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,25 @@
 import express from "express";
-import { usersController } from "../controllers/usersCollection.js";
+import {
+  deleteUserController,
+  getUsersController,
+  updateUserController,
+  usersController,
+} from "../controllers/usersCollection.js";
+import { verifyFBTokenController } from "../middlewares/verifyFBToken.middlewares.js";
 
 const router = express.Router();
 
 // users
 router.post("/register", usersController);
+
+router.get("/users", verifyFBTokenController, getUsersController);
+
+router.patch("/update-user/:id", verifyFBTokenController, updateUserController);
+
+router.delete(
+  "/delete-user/:id",
+  verifyFBTokenController,
+  deleteUserController
+);
 
 export default router;
